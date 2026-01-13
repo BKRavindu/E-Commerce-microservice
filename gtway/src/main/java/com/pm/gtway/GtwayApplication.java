d519459ec80d317af7157127ebfdb4041fe253a6
BKRavindu <bkravindu2000@gmail.com>
2026-01-13 22:02:20 +0530

    add security config

---
+++ gtway/src/main/java/com/pm/gtway/GtwayApplication.java
@@ -0,0 +1,13 @@
+package com.pm.gtway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtwayApplication.class, args);
+    }
+
+}
